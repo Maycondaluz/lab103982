9043988a5f0fe1bb45b24789698ef8c0316078fb
Maycon Vinicius da Luz <maycondaluz2010@hotmail.com>
2016-08-14 22:41:09 -0300

    #7

---
+++ lab103982/src/main/java/br/univel/mdb/MdbLogistica.java
@@ -0,0 +1,37 @@
+package br.univel.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+@MessageDriven(name = "MdbLog", activationConfig = {
+	    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/VENDATopic"),
+	    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+	    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
+	    })
+public class MdbLogistica implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbLogistica.class.toString());
+
+	@Override
+	public void onMessage(Message rcvMessage) {
+		// TODO Auto-generated method stub
+
+		ObjectMessage msg = null;
+		try {
+
+			LOGGER.warning("Iniciando!");
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LOGGER.warning("Desapachado.");
+
+	}
+
+}
